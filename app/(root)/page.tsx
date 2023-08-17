63beaf3f5aebc360e5dc6ee5b732b60d03e1a2ec
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-17 19:15:21 +0545

    Working on Design Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,156 @@
-import About from "@/app/sections/About";
+
 import Contact from "@/app/sections/Contact";
 import Design from "@/app/sections/Design";
-import Skill from "@/app/sections/Skill";
+
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <About />
-      <Skill />
+      <div className="flex w-[100%] min-h-[200vh] relative overflow-hidden">
+        <div className="w-[48%] min-h-[200vh] items-end pl-[10%] pr-[5%]  py-[10%] flex flex-col justify-between bg-light-400">
+          <Image
+            src={
+              "https://images.unsplash.com/photo-1581281863883-2469417a1668?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z29yaWxsYXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+            }
+            alt="Profile Photo"
+            width={600}
+            height={100}
+            className="object-cover h-[450px] w-[350px] rounded-lg"
+          />
+          <div className="flex flex-wrap gap-10 min-h-[100vh] py-[48%]">
+            <div className="flex flex-col items-center">
+              <Image
+                src={
+                  "https://images.unsplash.com/photo-1581281863883-2469417a1668?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z29yaWxsYXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+                }
+                alt="Profile Photo"
+                width={600}
+                height={100}
+                className="object-cover h-[120px] w-[120px] rounded-lg"
+              />
+              <p className="text-xs font-mb text-dark-700">Figma</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <Image
+                src={
+                  "https://images.unsplash.com/photo-1581281863883-2469417a1668?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z29yaWxsYXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+                }
+                alt="Profile Photo"
+                width={600}
+                height={100}
+                className="object-cover h-[120px] w-[120px] rounded-lg"
+              />
+              <p className="text-xs font-mb text-dark-700">Figma</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <Image
+                src={
+                  "https://images.unsplash.com/photo-1581281863883-2469417a1668?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z29yaWxsYXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+                }
+                alt="Profile Photo"
+                width={600}
+                height={100}
+                className="object-cover h-[120px] w-[120px] rounded-lg"
+              />
+              <p className="text-xs font-mb text-dark-700">Figma</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <Image
+                src={
+                  "https://images.unsplash.com/photo-1581281863883-2469417a1668?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z29yaWxsYXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+                }
+                alt="Profile Photo"
+                width={600}
+                height={100}
+                className="object-cover h-[120px] w-[120px] rounded-lg"
+              />
+              <p className="text-xs font-mb text-dark-700">Figma</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <Image
+                src={
+                  "https://images.unsplash.com/photo-1581281863883-2469417a1668?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z29yaWxsYXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+                }
+                alt="Profile Photo"
+                width={600}
+                height={100}
+                className="object-cover h-[120px] w-[120px] rounded-lg"
+              />
+              <p className="text-xs font-mb text-dark-700">Figma</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <Image
+                src={
+                  "https://images.unsplash.com/photo-1581281863883-2469417a1668?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z29yaWxsYXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+                }
+                alt="Profile Photo"
+                width={600}
+                height={100}
+                className="object-cover h-[120px] w-[120px] rounded-lg"
+              />
+              <p className="text-xs font-mb text-dark-700">Figma</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col justify-between py-[10%] px-7 w-[70%] absolute left-[48%] min-h-[190vh] bg-primary-700 gap-[15px] rounded-bl-[50%]">
+          <Image
+            src={"/assets/full_pattern.svg"}
+            width={600}
+            height={600}
+            alt="Hi"
+            className="absolute translate-x-[-40%] right-0 top-[0%] opacity-[0.2]"
+          />
+          <div className="flex flex-col justify-center gap-[15px] px-[5%]">
+            <Image
+              src={"/assets/HI.png"}
+              alt="Hi Photo"
+              width={120}
+              height={120}
+            />
+            <h2 className="font-libra font-eb text-light-400 text-3xl tracking-[1px]">
+              Preukti Parajuli
+            </h2>
+            <p className="text-light-400 font-l text-2xs w-[70%] leading-[180%]">
+              I am a passionate UI/UX Designer based in Kathmandu, Nepal. I
+              began my designing journey since 2023. This space is where
+              creativity and functionality unite. I believe that design is not
+              just about making things look pretty, but it's also about crafting
+              seamless and meaningful user experiences. Here you'll find a
+              collection of my latest projects. Feel free to explore my work and
+              get in touch if you'd like to collaborate. Let's create
+              exceptional experiences together!
+            </p>
+            <Link href={"/"}>
+              <button className="w-[150px] py-[8px] bg-light-400 hover:bg-light-500 rounded-md text-primary-700 font-sb text-2xs">
+                View my CV
+              </button>
+            </Link>
+          </div>
+          <div className="flex flex-col justify-center items-center w-[70%] pl-[10%] pb-[10%]">
+            <h2 className="font-sb text-light-400 text-[70px] leading-[90%]">
+              Skills
+            </h2>
+            <hr className="bg-light-400 w-[25%] h-[2px]" />
+            <ol className="w-[80%] ml-[5%] mt-[50px]">
+              <li className="text-light-400 font-l text-2xs leading-[120%]">
+                Great communication skills, teamwork capability, leadership
+                traits, prioritizing of tasks and confident.
+              </li>
+              <li className="text-light-400 font-l mt-[20px] text-2x leading-[120%]">
+                Proficient in creating visually appealing and intuitive user
+                interfaces along with seamless user experience through
+                user-centered designs.
+              </li>
+              <li className="text-light-400 font-l mt-[20px] text-2xs leading-[120%]">
+                Familarity with wireframes and interactive prototypes to
+                visualize design concepts and demonstrate functionality.
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
       <Design />
       <Contact />
     </>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -26,11 +26,25 @@ function NavBar() {
   return (
     <div
       className={`flex w-[100%] items-center justify-center gap-[100px] py-5 fixed top-0 z-[5]  ${
-        !isScroll ? " bg-[rgb(254,243,254,0.05)]" : "backdrop-blur-[1px]  bg-[rgb(254,243,254,0.2)]"
+        !isScroll
+          ? " bg-[rgb(54,48,85,0.02)]"
+          : "backdrop-blur-[1px]  bg-[rgb(54,48,85,0.6)]"
       }`}
     >
-      <h2 className="font-r text-sm text-primary-700">About</h2>
-      <h2 className="font-r text-sm text-primary-700">Skills</h2>
+      <h2
+        className={`font-r text-sm${
+          !isScroll ? " text-primary-700" : " text-light-500"
+        }`}
+      >
+        About
+      </h2>
+      <h2
+        className={`font-r text-sm${
+          !isScroll ? " text-primary-700" : " text-light-500"
+        }`}
+      >
+        Skills
+      </h2>
       <h2 className="font-r text-sm text-light-500">Designs</h2>
       <h2 className="font-r text-sm text-light-500">Contact</h2>
     </div>
